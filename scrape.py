b5b3c378114e72b74cdc45a4734fe0f38edd272a
Noman Sadiq <noman.sadiq@maf.ae>
2023-06-01 19:00:56 +0400

    Added link column also to scrap

--- scrape.py
+++ scrape.py
@@ -30,12 +30,17 @@ def scrape_page(url, conn):
         price = listing.find('div', class_='sc-cmkc2d-7 sc-11jo8dj-4 iZrQA-D kuRbQC').text.strip()
         location = listing.find('span', class_='MuiTypography-root').text.strip()
         sqft = listing.find('div', {'data-testid': 'listing-size'}).text.strip()
+        link_div = listing.find('a', class_='sc-15l4r6f-1')
+        print(link_div)
+        link = link_div['href']
+        print(link)
+
 
 
 
         # Insert data into the SQLite database
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO properties (title, price, location,sqft) VALUES (?, ?, ?, ?)", (title, price, location, sqft))
+        cursor.execute("INSERT INTO properties (title, price, location,sqft,link) VALUES (?, ?, ?, ?,?)", (title, price, location, sqft, link))
         conn.commit()
 
 # Create SQLite database and table
@@ -45,7 +50,7 @@ cursor.execute("CREATE TABLE IF NOT EXISTS properties (title TEXT, price TEXT, l
 
 # Scrape all pages
 base_url = "https://dubai.dubizzle.com/property-for-rent/residential/apartmentflat/?bedrooms=1&page="
-total_pages = 403  # Modify this value based on the total number of pages to scrape
+total_pages = 1  # Modify this value based on the total number of pages to scrape
 
 for page in range(1, total_pages + 1):
     url = base_url + str(page)
